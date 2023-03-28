cf7ca1ba4b8781839915eb102201f8d28f8742dc
Anguie02 <1487186@senati.pe>
2023-03-27 19:02:39 -0500

    Números de 1 a 10

---
+++ Ciclo_doWhile/src/ciclo_dowhile/Ciclo_doWhile.java
@@ -0,0 +1,26 @@
+
+package ciclo_dowhile;
+
+
+public class Ciclo_doWhile {
+
+    
+    public static void main(String[] args) {
+        //+++++++++
+        int i = 1;
+        /*La sentencia que se encuentra dentro de DO se ejecuta 
+        independientement de una condicón, al menos una vez*/
+        do{
+            System.out.println(i);//imprime 1
+            i ++;
+        //Luego compara el numero
+        }while(i<=10);
+        
+    
+    }
+      
+        
+      
+    }
+    
+
