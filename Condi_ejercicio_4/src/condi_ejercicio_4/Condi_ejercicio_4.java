36a77255e2c080704bc3233d776386e60981ad0c
Anguie02 <1487186@senati.pe>
2023-08-28 19:13:11 -0500

    falta terminar

---
+++ Condi_ejercicio_4/src/condi_ejercicio_4/Condi_ejercicio_4.java
@@ -0,0 +1,24 @@
+/*
+ Pedir tres números y mostrarlos ordenados de mayor a menor
+ */
+package condi_ejercicio_4;
+
+import java.util.Scanner;
+
+
+public class Condi_ejercicio_4 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        //Definimos las variable
+        int numero1, numero2, numero3;
+        //Pedimos el ingreso de tres números
+        System.out.println("Ingrese el primer número");
+        System.out.println("Ingrese el segundo número");
+        System.out.println("Ingrese el tercer número ");
+        
+    }//fin
+    
+}//fin
