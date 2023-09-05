b03abf8df9c0dd737c2ab8a0d40841c400982ecf
Anguie02 <1487186@senati.pe>
2023-09-04 19:00:15 -0500

    Uso de parámetros

--- JAVA _POO/Retorno_Valor/src/Clases/Main.java
+++ JAVA _POO/Retorno_Valor/src/Clases/Main.java
@@ -1,11 +1,23 @@
 
 package Clases;
 
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] arg){
+        
+        //Creamos un objeto Scanner para pedir los números
+        Scanner entrada = new Scanner(System.in);
+        
+        //Pedimos el ingreso de números y los almacenamos
+        System.out.println("Ingrese el primer número: ");
+        int numero1= entrada.nextInt();
+        System.out.println("Ingrese el segundo número: ");
+        int numero2 = entrada.nextInt();
+        
         Operaciones ope = new Operaciones();//Creamoes el objeto
-        ope.sumar(); //Invocamos los métodos
+        ope.resta(numero1, numero2); //Pasamos los números ingresados como argumentos
         ope.mostrar();
     
 }
--- JAVA _POO/Retorno_Valor/src/Clases/Operaciones.java
+++ JAVA _POO/Retorno_Valor/src/Clases/Operaciones.java
@@ -6,29 +6,24 @@ import java.util.Scanner;
 
 public class Operaciones {
     //Atributos
-   int numero1;
-   int numero2;
+  
    int sumar;
    int resta;
    
    //Métodos
-   public void sumar(){
-       Scanner entrada = new Scanner(System.in);
-       System.out.println("Ingrese el primer número: ");
-       numero1 = entrada.nextInt();
-       System.out.println("Ingrese el segundo número: ");
-       numero2 = entrada.nextInt();
+   public void sumar(int numero1, int numero2){
+       
        
        sumar = numero1 + numero2;   
    }
    
-   public void resta(){
-       resta = numero1 + numero2;
+   public void resta(int numero1, int numero2){
+       resta = numero1 - numero2;
        
    }
    
    public void mostrar(){
-       System.out.println("La suma es: "+ sumar);
+       System.out.println("La resta es: "+ resta);
    }
    
     
