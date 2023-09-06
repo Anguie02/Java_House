cdf5ea9272617d2afdd6ce0f4994534856b89b97
Anguie02 <1487186@senati.pe>
2023-09-05 19:02:47 -0500

    Valores de retorno

--- JAVA _POO/Retorno_Valor/src/Clases/Main.java
+++ JAVA _POO/Retorno_Valor/src/Clases/Main.java
@@ -17,8 +17,17 @@ public class Main {
         int numero2 = entrada.nextInt();
         
         Operaciones ope = new Operaciones();//Creamoes el objeto
-        ope.resta(numero1, numero2); //Pasamos los números ingresados como argumentos
-        ope.mostrar();
+        
+        //Al llamar al método nos retorna un valor
+        //1.Debe ser almacenado para mostrarlo
+        int resta = ope.resta(numero1, numero2);
+        System.out.println("La resta es: "+ resta);
+        
+        //2.Lo mostramos directamente
+        System.out.println("La suma es:"+ ope.sumar(numero1, numero2));
+        
+        
+        
     
 }
     
--- JAVA _POO/Retorno_Valor/src/Clases/Operaciones.java
+++ JAVA _POO/Retorno_Valor/src/Clases/Operaciones.java
@@ -1,30 +1,24 @@
 
 package Clases;
 
-import java.util.Scanner;
-
 
 public class Operaciones {
     //Atributos
-  
-   int sumar;
-   int resta;
    
-   //Métodos
-   public void sumar(int numero1, int numero2){
-       
-       
-       sumar = numero1 + numero2;   
+   
+   //Métodos: RETORNO DE VALORES 
+    
+   /*Cambiamos el valor de retorno*/
+   public int sumar(int numero1, int numero2){// void --> int
+      int sumar = numero1 + numero2;
+      return sumar;
    }
    
-   public void resta(int numero1, int numero2){
-       resta = numero1 - numero2;
-       
+   public int resta(int numero1, int numero2){
+      int resta = numero1 - numero2; 
+      return resta;
    }
    
-   public void mostrar(){
-       System.out.println("La resta es: "+ resta);
-   }
    
     
 }
